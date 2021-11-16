fd98e5bcdb9a8ba5ac49c2ed3adfaf9754c82583
stees <steffano.esteves@gmail.com>
2021-11-16 11:53:18 -0300

    usoH

--- 11 - processamentos - arrumando os lotes.R
+++ 11 - processamentos - arrumando os lotes.R
@@ -19,8 +19,8 @@ listaCampos <- c(
 )
 
 # tabela de tradução de usos e padrões em usoH
-usos <- unique(temp2$TipoUso)
-padrões <- unique(temp2$TipoPadrão)
+#usos <- unique(temp2$TipoUso)
+#padrões <- unique(temp2$TipoPadrão)
 
 
 for (ano in ListaAnos){
@@ -53,20 +53,10 @@ for (ano in ListaAnos){
   temp <- temp %>%
     select( c(1:5,8,6,7,9:14,18,15:17) )
   
+  temp1 <-
   arquivo <- paste0( "./10 - processamentos/IPTU_" , ano , "_loteArrumado.csv.gz")
   
-  
-  
-  
-  # remover depois de consolidar o método
-  temp2 <- temp
-  temp <- temp[1:100000,]
-  
-  
-  
-  
-  
-  
+
   
   temp %>%
     mutate(
@@ -84,32 +74,38 @@ for (ano in ListaAnos){
             UsoPadrão = toupper(paste( TipoUso , TipoPadrão )),
             # resumindo em usoH
             UsoH = case_when(
+                              # residência e termina com A ou B / cortiço = 01
                               str_detect( UsoPadrão , "(^(RESID).*(A|B)$)|(^CORTIÇO)" ) == TRUE ~ "01 - Uso Residencial Horizontal Baixo Padrão",
-                              str_detect( UsoPadrão , "^(RESID).*(C)$" ) == TRUE ~ "02 - Uso Residencial Horizontal Médio Padrão",
+                              # residência e termina com C = 02, D/E/F = 03
+                              str_detect( UsoPadrão , "^(RESID|FLAT.*RESID).*(C)$" ) == TRUE ~ "02 - Uso Residencial Horizontal Médio Padrão",
                               str_detect( UsoPadrão , "^(RESID).*(D|E|F)$" ) == TRUE ~ "03 - Uso Residencial Horizontal Alto Padrão",
-                              str_detect( UsoPadrão , "^(APARTA|PR.*PREDOM.*RESID).*(A|B)$" ) == TRUE ~ "14 - Uso Residencial Vertical Baixo Padrão",
-                              str_detect( UsoPadrão , "^(APARTA|PR.*PREDOM.*RESID).*(C)$" ) == TRUE ~ "04 - Uso Residencial Vertical Médio Padrão",
-                              str_detect( UsoPadrão , "^(APARTA|PR.*PREDOM.*RESID).*(D|E|F)$" ) == TRUE ~ "05 - Uso Residencial Vertical Alto Padrão",
-                              str_detect( UsoPadrão , "^(LOJA|OUTR.*EDIF.*(COMER|SERV)|ESCRIT).*(HORIZ|BARRA)" ) == TRUE ~ "06 - Uso Comércio e Serviço Horizontal",
-                              str_detect( UsoPadrão , "^(LOJA|OUTR.*EDIF.*(COMER|SERV)|ESCRIT|PR.*ESCRIT).*(VERTI)" ) == TRUE ~ "07 - Uso Comércio e Serviço Vertical",
-                              str_detect( UsoPadrão , "^(IND).*(HORIZ|BARRA|ARMAZ)" ) == TRUE ~ "08 - Uso Industrial",
+                              # apartamentos, prédio predom. resid. com A ou B = 14
+                              str_detect( UsoPadrão , "(APARTA|PR.*PREDOM.*RESID).*(A|B)$" ) == TRUE ~ "14 - Uso Residencial Vertical Baixo Padrão",
+                              # apartamentos, prédio predom. resid. ou flat com C = 04, D/E/F = 05
+                              str_detect( UsoPadrão , "(APARTA|PR.*PREDOM.*RESID|FLAT.*RESID).*(C)$" ) == TRUE ~ "04 - Uso Residencial Vertical Médio Padrão",
+                              str_detect( UsoPadrão , "(APARTA|PR.*PREDOM.*RESID|FLAT.*RESID).*(D|E|F)$" ) == TRUE ~ "05 - Uso Residencial Vertical Alto Padrão",
+                              # loja, posto de gasolina, outras edificações de comércio ou serviços, escritórios, horizontais ou em barracão = 06
+                              str_detect( UsoPadrão , "^(LOJA|OFICI|POSTO|OUTR.*EDIF.*(COMER|SERV)|.*ESCRIT).*(HORIZ|BARRA|ARMAZ)" ) == TRUE ~ "06 - Uso Comércio e Serviço Horizontal",
+                              # loja, outras edificações de comércio ou serviços, escritórios verticais = 07
+                              str_detect( UsoPadrão , "^(LOJA|OUTR.*EDIF.*(COMER|SERV)|FLAT.*COMER|.*ESCRIT).*(VERTI)" ) == TRUE ~ "07 - Uso Comércio e Serviço Vertical",
+                              str_detect( UsoPadrão , "^(IND).*(HORIZ|BARRA|ARMAZ|IND)" ) == TRUE ~ "08 - Uso Industrial",
                               str_detect( UsoPadrão , "^(ARMAZ).*(HORIZ|BARRA|ARMAZ)" ) == TRUE ~ "09 - Uso Armazéns e Depósitos",
-                              str_detect( UsoPadrão , "HOTEL|HOSPITAL|ASILO" ) == TRUE ~ "10 - Uso Especial ( Hotel, Hospital, Cartório, Etc. )",
+                              str_detect( UsoPadrão , "HOTEL|HOSPITAL|ASILO|ESTAÇÃO" ) == TRUE ~ "10 - Uso Especial ( Hotel, Hospital, Cartório, Etc. )",
                               str_detect( UsoPadrão , "ESCOLA" ) == TRUE ~ "11 - Uso Escola",
                               str_detect( UsoPadrão , "TEATRO|TEMPLO|CINEMA|CLUBE" ) == TRUE ~ "12 - Uso Coletivo ( Cinema, Teatro, Clube, Templo, Etc. )",
                               str_detect( UsoPadrão , "^TERRE" ) == TRUE ~ "13 - Terrenos Vagos",
-                              str_detect( UsoPadrão , "^GARAG.*(HORIZ|BARRA)" ) == TRUE ~ "15 - Uso garagens não-residenciais",
+                              str_detect( UsoPadrão , "^GARAG.*(HORIZ|BARRA|ARMAZ|EDIFÍCIO)" ) == TRUE ~ "15 - Uso garagens não-residenciais",
                               
                               TRUE ~ "FALTA FAZER, no final vai ser 99"
                             )
-          ) %>% View()
+          ) %>% #filter( str_detect(UsoH,"vai ser 99") == TRUE ) %>% View()
     select( c(-UsoPadrão)) %>%
     group_by( SQL ) %>%
     mutate(
             # consolidando um valor por lote/condomínio, mas ainda com várias instâncias, pra depois poder resumir os usos
             Construído = sum(Construído),
             across( Frentes:FatorObsoles, median ),
-            # resumindo os usos
+            # resumindo os usos APENAS dos condomínios
             
           ) %>%
     ungroup() %>%
@@ -121,8 +117,8 @@ for (ano in ListaAnos){
             TO_lote = Ocupado/Terreno,
             SQ = str_sub(SQL,0,6),
             ano = ano,
-          ) %>% View()
-  #write_csv2( file = arquivo )
+          ) %>%
+  write_csv2( file = arquivo )
   
   rm(temp)
   gc()
